9281076743457218f81f25dad4a8f900033a4591
hnakano863 <notchi863@gmail.com>
2020-04-01 23:48:25 +0900

    bug fix.

--- src/datasets.jl
+++ src/datasets.jl
@@ -4,9 +4,9 @@ function datasets(package_name::AbstractString)
 end
 
 function datasets()
-    if RDatasets.__datasets === nothing
+    if MyRDatasets.__datasets === nothing
         path = joinpath(DATASETS_DIR, "datasets.csv")
         global __datasets = CSV.read(path)
     end
-    return RDatasets.__datasets::DataFrame
+    return MyRDatasets.__datasets::DataFrame
 end
